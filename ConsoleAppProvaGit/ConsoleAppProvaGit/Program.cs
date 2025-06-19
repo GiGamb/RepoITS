53e8af4bb404eb556e26eede7cf5e34ede87f98d
SpicyTori <ginevra.gamberi@gmail.com>
2025-06-19 11:37:03 +0200

    ok jo

---
+++ ConsoleAppProvaGit/ConsoleAppProvaGit/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleAppProvaGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
